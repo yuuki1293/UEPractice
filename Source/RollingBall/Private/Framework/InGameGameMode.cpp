f7fbd925c3ce6d433d0e1af6a531e859d72c2c7f
yuuki1293 <yuuki1293@outlook.jp>
2025-05-12 09:21:34 +0900

    死亡判定を追加

--- Source/RollingBall/Private/Framework/InGameGameMode.cpp
+++ Source/RollingBall/Private/Framework/InGameGameMode.cpp
@@ -3,8 +3,42 @@
 
 #include "Framework/InGameGameMode.h"
 #include "Character/BallPlayer.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerStart.h"
 
 AInGameGameMode::AInGameGameMode()
 {
 	DefaultPawnClass = ABallPlayer::StaticClass();
+}
+
+void AInGameGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Viewportに配置されたPlayerStartを探す
+	const APlayerStart* PlayerStart = Cast<APlayerStart>(UGameplayStatics::GetActorOfClass(GetWorld(), APlayerStart::StaticClass()));
+
+	// PlayerStartの位置情報をRespornの位置として保持する
+	SpawnTransform = PlayerStart->GetActorTransform();
+}
+
+void AInGameGameMode::RespawnPlayer()
+{
+	// BallPlayerをSpawnする
+	FActorSpawnParameters SpawnInfo;
+	SpawnInfo.Instigator = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	ABallPlayer* Player = GetWorld()->SpawnActor<ABallPlayer>(ABallPlayer::StaticClass(), SpawnTransform, SpawnInfo);
+
+	// Controllerを設定する
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	PlayerController->Possess(Player);
+}
+
+void AInGameGameMode::KillPlayer(ABallPlayer* Player)
+{
+	// Playerを破棄する
+	Player->Destroy();
+
+	// Respawnを行う
+	RespawnPlayer();
 }
--- Source/RollingBall/Public/Framework/InGameGameMode.h
+++ Source/RollingBall/Public/Framework/InGameGameMode.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "Character/BallPlayer.h"
 #include "InGameGameMode.generated.h"
 
 /**
@@ -17,4 +18,19 @@ class ROLLINGBALL_API AInGameGameMode : public AGameModeBase
 public:
     // Constructor
     AInGameGameMode();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// PlayerをRespawnする位置情報
+	FTransform SpawnTransform;
+
+	/** Playerを破棄する **/
+	void KillPlayer(ABallPlayer* Player);
+
+private:
+	/** PlayerをRespawnする **/
+	void RespawnPlayer();
 };
